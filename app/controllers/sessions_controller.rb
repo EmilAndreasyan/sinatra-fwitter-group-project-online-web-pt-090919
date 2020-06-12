071adfd279afe068b87462f456352dfe8d936e69
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-12 06:49:22 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
   get '/login' do 
     user = User.find_by(email: params[:user][:email])
+    if params[:user][:email].empty? || params[:user][:password].empty?
+      
   end
   
   post '/logout' do
