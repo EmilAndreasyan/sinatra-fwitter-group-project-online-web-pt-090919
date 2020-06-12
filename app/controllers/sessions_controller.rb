5f2e08bc93e92b5f6f217c1f8b933c7d61e7d740
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-12 06:52:02 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     elsif user && user.authenticate(params[:user][:password])
     user.save
     session[:user_id] = user.id
+    redirect '/users/#{}'
   end
   
   post '/logout' do
