7a48c9502351557dad979027dbcca562dc6d7700
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-12 07:22:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable: sessions
-    set: sessions_secret
+    enable :sessions
+    set :sessions_secret, enigma
   end
   
   get '/' do
