18a7d321be076fbb2a05fb0ccd1495d98bf8f8b5
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-12 06:49:51 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,9 @@ class SessionsController < ApplicationController
   get '/login' do 
     user = User.find_by(email: params[:user][:email])
     if params[:user][:email].empty? || params[:user][:password].empty?
-      
+      @error = "Email and password fields cannot be blank*"
+      erb :'users/login' 
+    
   end
   
   post '/logout' do
